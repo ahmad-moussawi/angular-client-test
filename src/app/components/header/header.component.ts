91d0f7a3786c13b58d5b8c1a64dadef2a7d084ea
Hussam Al-Tayeb <ht990332@gmx.com>
2024-03-04 18:49:46 +0200

    Update header.component.ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnChanges, OnInit, SimpleChanges, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs/internal/Observable';
-import { HttpResponse } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
-import { tap } from 'rxjs/internal/operators/tap';
+import { HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -14,25 +13,28 @@ import { tap } from 'rxjs/internal/operators/tap';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent implements OnInit {
 
+export class HeaderComponent implements OnInit {
   authService = inject(AuthService)
 
   isLoggedIn$ = new BehaviorSubject<Boolean>(false);
 
+  authenticationStatus!: Observable<HttpResponse<object>>;
+
+  constructor() {
+    this.authenticationStatus = this.authService.authStatus()
+
+  }
   ngOnInit(): void {
-    console.log('IS INIT');
-    this.authService.authStatus().pipe(
-      tap( console.log ),
-      tap( res => {
-          this.isLoggedIn$.next(!!res?.body)
-      } )
-    ).subscribe(
-      () => {},
-      ( err ) => {
-        console.log( err );
-        this.isLoggedIn$.next( false );
+    this.authenticationStatus.subscribe({
+      next: (res: any) => {
+        if (res.body) {
+          this.isLoggedIn$.next(true)
+        } else this.isLoggedIn$.next(false)
+      },
+      error: (err) => {
+        this.isLoggedIn$.next(false)
       }
-    )
+    });
   }
 }
